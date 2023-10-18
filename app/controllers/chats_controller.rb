a6ec8a475391f54006df9ef2a8c4e8dd0308be85
Viktor Miric <viktorm47@gmail.com>
2023-10-18 16:27:11 +0200

    Get method for all current user chat's with access token authentication

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,3 +1,12 @@
 class ChatsController < ApplicationController
-    
+  skip_before_action :verify_authenticity_token, raise: false
+  before_action :authenticate_devise_api_token!
+  
+  def get_all_chats
+    if current_devise_api_token
+      render json: current_devise_api_token.resource_owner.chats
+    else
+      reander json: { message: 'error' }, status: :unauthorized
+    end
+  end
 end
